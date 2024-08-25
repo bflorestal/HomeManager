b27b3761403fccb3362834c360523a47d2d361ea
bflorestal <yayou7710@hotmail.fr>
2024-08-25 21:45:25 +0200

    chore: adjusted base home command for first version

--- src/main/kotlin/fr/bflorestal/homeManager/commands/HomeCommand.kt
+++ src/main/kotlin/fr/bflorestal/homeManager/commands/HomeCommand.kt
@@ -12,16 +12,7 @@ class HomeCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
         if (sender !is Player) return false
 
-        // TODO: Let the player teleport if they have only one home
-        if (args.isNullOrEmpty()) {
-            sender.sendMessage(Component.text("Vous devez spécifier un nom de home")
-                .color(TextColor.fromHexString("#FF0000")))
-            return false
-        }
-
-        val homeName = args[0]
-
-        sender.sendMessage(Component.text("Téléportation vers ${homeName}...")
+        sender.sendMessage(Component.text("Téléportation en cours...")
             .color(TextColor.fromHexString("#00FF00")))
 
         sender.teleport(sender.world.spawnLocation)
